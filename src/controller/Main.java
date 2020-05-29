4faca9d2493c2695b18a34cfdec1c772366bf04f
saiteja8848 <saiteja8848@gmail.com>
2020-05-29 17:11:40 +0530

    FSJ0420A102

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,1 +1,15 @@
+package controller;
 
+import java.sql.SQLException;
+
+import dao.SkillDAO;
+
+public class Main{
+	public static void main(String args[]) throws SQLException {
+   
+		SkillDAO skillDao = new SkillDAO();
+		skillDao.skillCount();
+	
+				
+	}
+}
--- src/dao/SkillDAO.java
+++ src/dao/SkillDAO.java
@@ -1,1 +1,45 @@
+package dao;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.TreeMap;
+
+import model.Skill;
+import utility.ConnectionManager;
+
+public class SkillDAO {
+
+	public TreeMap<Skill, Integer> skillCount() throws SQLException {
+		Connection connection = ConnectionManager.getConnection();
+
+		if (connection != null) {
+			Statement statement = connection.createStatement();
+			String sql = "SELECT * FROM SKILL";
+			ResultSet rs = statement.executeQuery(sql);
+			while (rs.next()) {
+				System.out.println(rs.getString(2)+"-"+count(rs.getInt(1)));
+			}
+
+		}
+		return null;
+	}
+	
+	public int count(int id) throws SQLException {
+		
+		int count=0;
+		Connection connection = ConnectionManager.getConnection();
+		if(connection!=null) {
+			
+			Statement stmt = connection.createStatement();
+			String sql = "select count(sid) from cricketplayers where sid ="+id;
+			ResultSet rs = stmt.executeQuery(sql);			
+			while (rs.next()) {
+				count = Integer.parseInt(rs.getString("count(sid)"));				
+			}
+		 
+		}
+		return count;
+	}
+}
